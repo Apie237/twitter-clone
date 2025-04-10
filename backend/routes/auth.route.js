8dbc3a4fc0f2b4a69acd7080bd8a3f4ffacc4b9c
Apie-237 <cheforsylvanus@gmail.com>
2025-04-10 11:33:49 +0400

    1- auth controllers added

---
+++ backend/routes/auth.route.js
@@ -0,0 +1,14 @@
+import express from "express"
+import { signup ,login, logout, getMe } from "../controllers/auth.controllers.js";
+import { protectRoute } from "../middleware/protect.route.js";
+
+const router = express.Router();
+router.get("/me", protectRoute, getMe)
+
+router.post("/signup",signup)
+
+router.post("/login",login)
+
+router.post("/logout",logout)
+
+export default router
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
-import AuthRoutes from "./routes/auth.routes.js"
+import authRoutes from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 import connectMongoDB from "./db/connectMongoDB.js"
 
 
@@ -8,7 +9,14 @@ dotenv.config()
 const app = express();
 const PORT = process.env.PORT || 5000
 
-app.post("/api/auth", AuthRoutes)
+app.use(express.json()) // to parse req.body
+// limit shouldn't be too high to prevent DOS
+app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
+
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes)
+
 
 
 app.listen(PORT, ()=> {
